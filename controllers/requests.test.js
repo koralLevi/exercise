0f2d21d09578d16d35a4fe68d288304da50c10bf
koral <korallevi20@gmail.com>
2021-10-02 15:10:36 +0300

    add api tests

--- controllers/requests.test.js
+++ controllers/requests.test.js
@@ -1,66 +1,23 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
 let app = require('../index');
-let requestsController = require('./requests')(app);
-let should = chai.should();
+require('./requests')(app);
 const request = require('supertest');
 
 
-chai.use(chaiHttp);
+describe('API - resource', function () {
 
-
-
-describe('GET /resource', function() {
-    it('responds with json', function(done) {
-      request(app)
-        .get('/api/resource')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
+    it('it should responds with json', function (done) {
+        request(app)
+            .get('/api/resource')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
-  });
 
-
-// describe('Requests test', () => {
-//     // beforeEach((done) => { //Before each test we empty the database
-
-//     // });
-//     /*
-//       * Test the /GET route
-//       */
-//     // describe('/GET last resource', () => {
-//         //   it('it should return a value', (done) => {
-//         //     chai.request(server)
-//         //         .get('/book')
-//         //         .end((err, res) => {
-//         //               res.should.have.status(200);
-//         //               res.body.should.be.a('array');
-//         //               res.body.length.should.be.eql(0);
-//         //           done();
-//         //         });
-//         //   });
-//     // });
-
-//     // describe('/POST resource', () => {
-//     //     it('it should return success message', (done) => {
-//     //         let data = {
-//     //             name: "Dan",
-//     //             lastname: "Oz"
-//     //         }
-//     //         // { message: "success" }
-//     //         chai.request(requestsController)
-//     //             .post('/api/resource')
-//     //             .send(data)
-//     //             .end((err, res) => {
-//     //                 console.log("postttttttttt",res)
-//     //                 // res.should.have.status(200);
-//     //                 // res.body.should.be.a('object');
-//     //                 // res.body.should.have.property('message');
-//     //                 done();
-//     //             });
-//     //         done();
-//     //     });
-
-//     // });
-
-// });
+    it('it should return a success message', function (done) {
+        request(app)
+            .post('/api/resource')
+            .send('name=john') 
+            .set('Accept', 'application/json')
+            .expect(200, { message: 'success' }, done);
+    });
+});
